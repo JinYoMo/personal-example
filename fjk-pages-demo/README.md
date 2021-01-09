27d4932d4f5b1c6f79666672e34291a4d3d7b7cf
jkfan2 <jkfan2@iflytek.com>
2021-01-09 17:09:17 +0800

    使用封装自动化构建工作流fjk-pages的Gulp-cli 完成新项目的构建使用案例

---
+++ fjk-pages-demo/README.md
@@ -0,0 +1,10 @@
+# fjk-pages-demo
+
+## 使用封装自动化构建工作流fjk-pages的Gulp-cli 完成新项目的构建使用案例
+
+# 安装依赖
+* yarn init --yes  生成package.js文件
+* yarn add fjk-pages --dev  安装fjk-pages依赖
+
+# 运行方式
+* yarn build  完成构建
---
+++ fjk-pages-demo/pages.config.js
@@ -0,0 +1,56 @@
+ module.exports ={
+   //在此处可动态配置打包文件路径
+    build: {
+      src: 'src',
+      dist: 'release',
+      temp: '.tmp',
+      public: 'public',
+      paths: {
+        styles: 'assets/styles/*.scss',
+        scripts: 'assets/scripts/*.js',
+        pages: '*.html',
+        images: 'assets/images/**',
+        fonts: 'assets/fonts/**'
+      }
+    },
+   data:{
+      menus: [
+        {
+          name: 'Home',
+          icon: 'aperture',
+          link: 'index.html'
+        },
+        {
+          name: 'Features',
+          link: 'features.html'
+        },
+        {
+          name: 'About',
+          link: 'about.html'
+        },
+        {
+          name: 'Contact',
+          link: '#',
+          children: [
+            {
+              name: 'Twitter',
+              link: 'https://twitter.com/w_zce'
+            },
+            {
+              name: 'About',
+              link: 'https://weibo.com/zceme'
+            },
+            {
+              name: 'divider'
+            },
+            {
+              name: 'About',
+              link: 'https://github.com/zce'
+            }
+          ]
+        }
+      ],
+      pkg: require('./package.json'),
+      date: new Date()
+  }
+ } 
